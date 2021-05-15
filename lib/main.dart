30e8266d3b57cbf6f7c786730462be96a496d4f6
Shubham Kumar <developer.shubham07@gmail.com>
2021-05-15 17:54:21 +0530

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+
     return MaterialApp(
       home: Material(
         child: Center(
           child: Container(
-            child: Text("Welcome to My Flutter App"),
+            child: Text("Welcome to My Flutter App. It's a $days app!"),
           ),
         ),
       ),
