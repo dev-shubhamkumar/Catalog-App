a2dcd61c30590f01d84e48b6537429cc045e6f29
Shubham Kumar <developer.shubham07@gmail.com>
2021-05-16 17:52:24 +0530

    Added Login Screen

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,15 +4,74 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+
+          SizedBox(
+            height: 40,
           ),
-        ),
+
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+
+          SizedBox(
+            height: 20,
+          ),
+          
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          SizedBox(
+            height: 20,
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: <Widget>[
+
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+
+                SizedBox(
+                  height: 20,
+                ),
+
+                ElevatedButton(
+                  onPressed: () {
+                    print("Login Button Testing!!!");
+                  }, 
+                  child: Text(
+                    "Login"
+                  ),
+                  style: TextButton.styleFrom(),
+                ),
+                
+              ],
+            ),
+          ),
+
+        ],
       ),
     );
   }
